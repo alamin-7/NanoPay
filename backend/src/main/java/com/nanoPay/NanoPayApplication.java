fd18f92c57198aa4a625897dcce139d2c934d27a
alamin-7 <alamin188914@gmail.com>
2025-05-21 00:28:24 +0600

    environment variable read from .env file using ApplicationContextInitializer

--- backend/src/main/java/com/nanoPay/NanoPayApplication.java
+++ backend/src/main/java/com/nanoPay/NanoPayApplication.java
@@ -1,11 +1,17 @@
 package com.nanoPay;
 
+import com.nanoPay.config.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NanoPayApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(NanoPayApplication.class, args);
+		//SpringApplication.run(NanoPayApplication.class, args);
+
+		SpringApplication app = new SpringApplication(NanoPayApplication.class);
+		app.addInitializers(new DotenvInitializer());
+		app.run(args);
+
 	}
 }

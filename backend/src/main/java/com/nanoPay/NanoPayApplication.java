8e253c06e393a9fe0e5af240727db76c2c3811bb
alamin-7 <alamin188914@gmail.com>
2025-05-22 00:13:31 +0600

    dockerized the application with dev and production profile.

--- backend/src/main/java/com/nanoPay/NanoPayApplication.java
+++ backend/src/main/java/com/nanoPay/NanoPayApplication.java
@@ -7,11 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NanoPayApplication {
 	public static void main(String[] args) {
-		//SpringApplication.run(NanoPayApplication.class, args);
+		SpringApplication.run(NanoPayApplication.class, args);
 
-		SpringApplication app = new SpringApplication(NanoPayApplication.class);
+/*		SpringApplication app = new SpringApplication(NanoPayApplication.class);
 		app.addInitializers(new DotenvInitializer());
-		app.run(args);
+		app.run(args);*/
 
 	}
 }
